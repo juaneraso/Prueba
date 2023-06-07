615deffaa31a43be5a38128428b403705d0e3237
Juan <juangui9508@hotmail.com>
2023-06-07 10:40:51 -0500

    HOLA

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function BinarioADecimal(num) {
+ 
+ let amigo = "hola";
+
+}
+
+function DecimalABinario(num) {
+
+var feo = "HOla";
+}
+
+module.exports = {
+   BinarioADecimal,
+   DecimalABinario,
+};
